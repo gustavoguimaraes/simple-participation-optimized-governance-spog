4de23b612bb0266b74f11ea306bed5a45d3494e2
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2022-12-11 18:43:38 +0100

    Init SPOG tests

---
+++ test/SPOG.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import "forge-std/Test.sol";
+import "../src/SPOG.sol";
+
+contract SPOGTest is Test {
+    SPOG public spog;
+
+    function setUp() public {
+        // spog = new SPOG();
+    }
+
+    function testStake() public {}
+
+    function testUnstake() public {}
+}
